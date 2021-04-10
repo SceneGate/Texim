fd95fb18cc2f436bfa07d9f16f92be4436a7b248
Benito Palacios Sanchez <benito356@gmail.com>
2021-04-10 13:09:22 +0000

    :sparkles: Implemennt color encoding and BGR555

---
+++ src/Texim/Palettes/Bgr555.cs
@@ -0,0 +1,114 @@
+// Copyright (c) 2021 SceneGate
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+namespace Texim.Palettes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class Bgr555 : IColorEncoding
+    {
+        private const int BytesPerColor = 2;
+
+        public static Rgb FromUInt16(ushort value)
+        {
+            return new Rgb {
+                Red = (byte)((value & 0x1F) << 3),
+                Green = (byte)(((value >> 5) & 0x1F) << 3),
+                Blue = (byte)(((value >> 10) & 0x1F) << 3),
+                Alpha = 255,
+            };
+        }
+
+        public static ushort ToUInt16(Rgb color)
+        {
+            return (ushort)((color.Red >> 3)
+                | ((color.Green >> 3) << 5)
+                | ((color.Blue >> 3) << 10));
+        }
+
+        public Rgb Decode(Stream stream)
+        {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
+            ushort data = (ushort)(stream.ReadByte() | (stream.ReadByte() << 8));
+            return FromUInt16(data);
+        }
+
+        public Rgb[] Decode(Stream stream, int numColors)
+        {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+            if (numColors < 0)
+                throw new ArgumentOutOfRangeException(nameof(numColors), numColors, "Invalid arg");
+
+            byte[] buffer = new byte[numColors * BytesPerColor];
+            int read = stream.Read(buffer, 0, buffer.Length);
+            if (read != buffer.Length) {
+                throw new EndOfStreamException();
+            }
+
+            return Decode(buffer);
+        }
+
+        public Rgb[] Decode(Span<byte> data)
+        {
+            int numColors = data.Length / BytesPerColor;
+            var colors = new Rgb[numColors];
+            for (int i = 0; i < numColors; i++) {
+                ushort value = (ushort)(data[i * 2] | (data[(i * 2) + 1] << 8));
+                colors[i] = FromUInt16(value);
+            }
+
+            return colors;
+        }
+
+        public void Encode(Stream stream, Rgb color)
+        {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
+            ushort data = ToUInt16(color);
+            stream.WriteByte((byte)(data & 0xFF));
+            stream.WriteByte((byte)(data >> 8));
+        }
+
+        public void Encode(Stream stream, IEnumerable<Rgb> colors)
+        {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+            if (colors == null)
+                throw new ArgumentNullException(nameof(colors));
+
+            var colorList = colors.ToArray();
+            byte[] buffer = new byte[colorList.Length * 2];
+
+            for (int i = 0; i < colorList.Length; i++) {
+                ushort data = ToUInt16(colorList[i]);
+                buffer[i * 2] = (byte)(data & 0xFF);
+                buffer[(i * 2) + 1] = (byte)(data >> 8);
+            }
+
+            stream.Write(buffer, 0, buffer.Length);
+        }
+    }
+}
---
+++ src/Texim/Rgb.cs
@@ -0,0 +1,44 @@
+// Copyright (c) 2021 SceneGate
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+namespace Texim
+{
+    using System.Drawing;
+
+    public readonly struct Rgb
+    {
+        public Rgb(byte red, byte green, byte blue, byte alpha)
+        {
+            Alpha = alpha;
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+
+        public byte Red { get; init; }
+
+        public byte Green { get; init; }
+
+        public byte Blue { get; init; }
+
+        public byte Alpha { get; init; }
+
+        public readonly Color ToColor() => Color.FromArgb(Alpha, Red, Green, Blue);
+    }
+}
